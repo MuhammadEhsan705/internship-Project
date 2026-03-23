08f917a945de9bd4fc95cbc12d31224bf5bf5a78
MuhammadEhsan705 <muhammadehsanali103@gmail.com>
2026-03-23 01:45:54 -0700

    add ham burger

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,126 +1,221 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
-import { Link } from 'react-router-dom';
-import { Calendar, LogOut, Package, ShoppingCart, UserCircle } from "lucide-react"
-import toast from 'react-hot-toast';
- 
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { Link } from "react-router-dom";
+import {
+  Calendar,
+  LogOut,
+  Package,
+  ShoppingCart,
+  UserCircle,
+} from "lucide-react";
+import toast from "react-hot-toast";
+
 const Navbar = () => {
-    const { navigate, user, setUser,axios,Cartcount } = useContext(AppContext);
-    const [isMenuOpen] = useState(false)
-    const [isProfileOpen, setIsProfileOpen] = useState(false)
-    const logout=async()=>{
-        try {
-           const{data}=await axios.post("/api/auth/logout");
-           if(data.success){
-            setUser(null);
-            toast.success(data.message);
-            navigate("/");
-           }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <nav className='bg-cyan-50 shadow-md sticky top-0 z-50 py-3'>
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-                <div className=' flex  items-center justify-between h-16'>
-                    {/* left logo */}
-                    <div className='flex items-center '>
-                        <Link to="/" className='text-2xl font-bold text-blue-600'>
-
-                            <img src='./logo.png' alt="logo" className='w-32' />
-                        </Link>
-                    </div>
-                    {/* menu items (desktop) */}
-                    <div className='hidden md:flex items-center space-x-8'>
-                        <Link to={'/'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Home</Link>
-                        <Link to={'/menu'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Menus</Link>
-                        <Link to={'/book-table'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Booke Tabel</Link>
-                        <Link to={'/contact'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Contact</Link>
-
-                    </div>
-                    {/* Right-Cart &login/profile */}
-                    <div className='flex items-center space-x-4'>
-                    {/* <button onClick={()=>navigate("/admin")} className='cursor-pointer bg-orange-500 hover:bg-orange-600 text-black font-semibold px-6 py-3 rounded-full transition-all duration-300'>Admin</button> */}
-                        <button onClick={() => navigate("/cart")} className='relative p-2 hover:bg-gray-100 rounded-lg git add .
+  const { navigate, user, setUser, axios, Cartcount } = useContext(AppContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const logout = async () => {
+    try {
+      const { data } = await axios.post("/api/auth/logout");
+      if (data.success) {
+        setUser(null);
+        toast.success(data.message);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <nav className="bg-cyan-50 shadow-md sticky top-0 z-50 py-3">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className=" flex  items-center justify-between h-16">
+          {/* left logo */}
+          <div className="flex items-center ">
+            <Link to="/" className="text-2xl font-bold text-blue-600">
+              <img src="./logo.png" alt="logo" className="w-32" />
+            </Link>
+          </div>
+          {/* menu items (desktop) */}
+          <div className="hidden md:flex items-center space-x-8">
+            <Link
+              to={"/"}
+              className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to={"/menu"}
+              className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+            >
+              Menus
+            </Link>
+            <Link
+              to={"/book-table"}
+              className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+            >
+              Booke Tabel
+            </Link>
+            <Link
+              to={"/contact"}
+              className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+            >
+              Contact
+            </Link>
+          </div>
+          {/* Right-Cart &login/profile */}
+          <div className="flex items-center space-x-4">
+            <button
+              onClick={() => navigate("/admin")}
+              className="cursor-pointer bg-orange-500 hover:bg-orange-600 text-black font-semibold px-6 py-3 rounded-full transition-all duration-300"
+            >
+              Admin
+            </button>
+            <button
+              onClick={() => navigate("/cart")}
+              className='relative p-2 hover:bg-gray-100 rounded-lg git add .
 git commit -m "update message"
-git push origin maintransition-colors'>
-                            <ShoppingCart size={22} className='text-gray-700' />
-                            <span className='absolute -top-1 -right-1 bg-orange-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center font-medium'>{Cartcount}</span>
-                        </button>
-                        <div className='hidden md:block '>
-                            {
-                                user ? (
-                                    <div className='relative'>
-                                        <button onMouseEnter={() => setIsProfileOpen(true)} onMouseLeave={() => setIsProfileOpen(false)} className='p-2 hover:bg-gray-100 rounded-lg transition-colors'>
-                                            <UserCircle size={30} className='text-gray-700' />
-                                        </button>
-                                        {
-                                            isProfileOpen && (
-                                                <div className='absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg  py-2 border border-gray-100' onMouseEnter={() => setIsProfileOpen(true)} onMouseLeave={() => setIsProfileOpen(false)} >
-
-                                                    <Link to={"/my-booking"} className='flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors'><Calendar size={18} className='mr-3' />
-                                                        My Bookings</Link>
-                                                    <Link to={"/my-orders"} className='flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors'><Package size={18} className='mr-3' />
-                                                        My Order</Link>
-                                                    <button onClick={logout} className='flex text-center w-full px-4 py-2 text-red-600 hover:bg-red-50 transition-colors'><LogOut size={18} className='mr-3' />
-                                                        Logout</button>
-                                                </div>
-                                            )
-                                        }
-                                    </div>
-                                ) : (
-                                    <button onClick={() => navigate("/login")} className='bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition-colors font-medium cursor-pointer'>Login </button>
-                                )
-                            }
-
-                        </div>
-
+git push origin maintransition-colors'
+            >
+              <ShoppingCart size={22} className="text-gray-700" />
+              <span className="absolute -top-1 -right-1 bg-orange-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center font-medium">
+                {Cartcount}
+              </span>
+            </button>
+            <div className="hidden md:block ">
+              {user ? (
+                <div className="relative">
+                  <button
+                    onMouseEnter={() => setIsProfileOpen(true)}
+                    onMouseLeave={() => setIsProfileOpen(false)}
+                    className="p-2 hover:bg-gray-100 rounded-lg transition-colors"
+                  >
+                    <UserCircle size={30} className="text-gray-700" />
+                  </button>
+                  {isProfileOpen && (
+                    <div
+                      className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg  py-2 border border-gray-100"
+                      onMouseEnter={() => setIsProfileOpen(true)}
+                      onMouseLeave={() => setIsProfileOpen(false)}
+                    >
+                      <Link
+                        to={"/my-booking"}
+                        className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
+                      >
+                        <Calendar size={18} className="mr-3" />
+                        My Bookings
+                      </Link>
+                      <Link
+                        to={"/my-orders"}
+                        className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
+                      >
+                        <Package size={18} className="mr-3" />
+                        My Order
+                      </Link>
+                      <button
+                        onClick={logout}
+                        className="flex text-center w-full px-4 py-2 text-red-600 hover:bg-red-50 transition-colors"
+                      >
+                        <LogOut size={18} className="mr-3" />
+                        Logout
+                      </button>
                     </div>
+                  )}
                 </div>
-                {/* mobile-menu */}
-                {
-                    isMenuOpen && (
-                        <div className='md:hidden py-4 border-t border-gray-200'>
-                            <div className='flex flex-col space-y-3'>
-                                <Link to={'/'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Home</Link>
-                                <Link to={'/menu'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Menus</Link>
-                                <Link to={'/contact'} className='text-gray-700 hover:text-blue-600 transition-colors font-medium'>Contact</Link>
-                                {
-                                    user ? (
-                                        <div className='relative'>
-                                            <button onMouseEnter={() => setIsProfileOpen(true)} onMouseLeave={() => setIsProfileOpen(false)} className='p-2 hover:bg-gray-100 rounded-lg transition-colors'>
-                                                <UserCircle size={30} className='text-gray-700' />
-                                            </button>
-                                            {
-                                                isProfileOpen && (
-                                                    <div className='absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg  py-2 border border-gray-100' onMouseEnter={() => setIsProfileOpen(true)} onMouseLeave={() => setIsProfileOpen(false)} >
-
-                                                        <Link to={"/my-booking"} className='flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors'><Calendar size={18} className='mr-3' />
-                                                            My Bookings</Link>
-                                                        <Link to={"/my-orders"} className='flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors'><Package size={18} className='mr-3' />
-                                                            My Order</Link>
-                                                        <button onClick={logout} className='flex text-center w-full px-4 py-2 text-red-600 hover:bg-red-50 transition-colors'><LogOut size={18} className='mr-3' />
-                                                            Logout</button>
-                                                    </div>
-                                                )
-                                            }
-                                        </div>
-                                    ) : (
-                                        <button onClick={() => navigate("/login")} className='bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition-colors font-medium cursor-pointer'>Login </button>
-                                    )
-                                }
-
-
-                            </div>
-
-
-                        </div>
-                    )
-                }
+              ) : (
+                <button
+                  onClick={() => navigate("/login")}
+                  className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition-colors font-medium cursor-pointer"
+                >
+                  Login{" "}
+                </button>
+              )}
             </div>
-        </nav>
-    );
-}
+          </div>
+        </div>
+        {/* mobile-menu */}
+        {isMenuOpen && (
+          <div className="md:hidden py-4 border-t border-gray-200">
+            <div className="md:hidden">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="p-2 rounded-lg hover:bg-gray-100 transition-colors"
+              >
+                {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+              </button>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <Link
+                to={"/"}
+                className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+              >
+                Home
+              </Link>
+              <Link
+                to={"/menu"}
+                className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+              >
+                Menus
+              </Link>
+              <Link
+                to={"/contact"}
+                className="text-gray-700 hover:text-blue-600 transition-colors font-medium"
+              >
+                Contact
+              </Link>
+              {user ? (
+                <div className="relative">
+                  <button
+                    onMouseEnter={() => setIsProfileOpen(true)}
+                    onMouseLeave={() => setIsProfileOpen(false)}
+                    className="p-2 hover:bg-gray-100 rounded-lg transition-colors"
+                  >
+                    <UserCircle size={30} className="text-gray-700" />
+                  </button>
+                  {isProfileOpen && (
+                    <div
+                      className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg  py-2 border border-gray-100"
+                      onMouseEnter={() => setIsProfileOpen(true)}
+                      onMouseLeave={() => setIsProfileOpen(false)}
+                    >
+                      <Link
+                        to={"/my-booking"}
+                        className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
+                      >
+                        <Calendar size={18} className="mr-3" />
+                        My Bookings
+                      </Link>
+                      <Link
+                        to={"/my-orders"}
+                        className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
+                      >
+                        <Package size={18} className="mr-3" />
+                        My Order
+                      </Link>
+                      <button
+                        onClick={logout}
+                        className="flex text-center w-full px-4 py-2 text-red-600 hover:bg-red-50 transition-colors"
+                      >
+                        <LogOut size={18} className="mr-3" />
+                        Logout
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <button
+                  onClick={() => navigate("/login")}
+                  className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-700 transition-colors font-medium cursor-pointer"
+                >
+                  Login{" "}
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
